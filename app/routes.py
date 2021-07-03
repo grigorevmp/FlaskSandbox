cf3d1967ee6a7e4ecd3b81ddee67c839049853c3
Mikhail <16112000m@gmail.com>
2021-07-03 11:59:45 +0300

    Initial commit

---
+++ app/routes.py
@@ -0,0 +1,23 @@
+from app import appName
+from flask import render_template
+
+
+@appName.route('/')
+@appName.route('/index')
+def index():
+    user = {'username': 'Михаил Григорьев'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+                'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'Ипполит'},
+            'body': 'Какая гадость эта ваша заливная рыба!!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
