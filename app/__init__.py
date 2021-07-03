338b9707b18df2094e93613cca437f6e72d5e3c2
Mikhail <16112000m@gmail.com>
2021-07-03 13:01:32 +0300

    - new

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
+from config import Config
 
 appName = Flask(__name__)
+appName.config.from_object(Config)
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import appName
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
+from app.forms import LoginForm
 
 
 @appName.route('/')
@@ -13,7 +14,7 @@ def index():
         },
         {
             'author': {'username': 'Susan'},
-                'body': 'The Avengers movie was so cool!'
+            'body': 'The Avengers movie was so cool!'
         },
         {
             'author': {'username': 'Ипполит'},
@@ -21,3 +22,13 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@appName.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
